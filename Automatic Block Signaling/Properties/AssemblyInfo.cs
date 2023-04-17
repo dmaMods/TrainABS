dd0731767354cd5dedeadd17e1f82fb040f55731
Martina Rajić <tina@git.castle.net>
2023-04-17 12:42:44 +0200

    version update

--- Automatic Block Signaling/Properties/AssemblyInfo.cs
+++ Automatic Block Signaling/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.125.1503")]
-[assembly: AssemblyFileVersion("1.0.125.1501")]
+[assembly: AssemblyVersion("1.10.203.1508")]
+[assembly: AssemblyFileVersion("1.10.203.1508")]
